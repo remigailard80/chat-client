b8acf5549309c792e9b97a69b973f9df887053cb
KIMMINSEOK <remigailard80@github.com>
2022-01-02 22:59:50 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useRef, useState, useEffect} from 'react';
+import io from 'socket.io-client'
+
 import './App.css';
 
+const socket = io('ws://localhost:8000', {
+  path: '/socket.io',
+  withCredentials: true,
+  transports: ['websocket']
+})
+
 function App() {
+  const chatInput = useRef();
+  const [messages, setMessages] = useState([]);
+
+  const handleChatButton = () => {
+    const chatRef = chatInput.current;
+
+    socket.emit('message', chatRef.value);
+  }
+  
+  useEffect(() => {
+    socket.on('message', (message) => {
+      console.log(message);
+      setMessages((messages) => [...messages, message])
+      chatInput.current.value = ''
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="chat-list">
+        {messages.map((item, idx) => <li key={idx}> {item} </li>)}
+      </ul>
+      <div className="button-wrapper"> 
+        <textarea placeholder="채팅창" ref={chatInput}/>
+        <button type="button" onClick={handleChatButton}> 전송 </button>
+      </div>
     </div>
   );
 }
